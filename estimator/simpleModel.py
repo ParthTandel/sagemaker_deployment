232239fd6d70a0dd301689d75598f7404eabdc59
parth <parthtandel0594@gmail.com>
2021-03-27 16:21:13 -0400

    model estimator ready

--- estimator/simpleModel.py
+++ estimator/simpleModel.py
@@ -2,8 +2,8 @@ import pickle
 import os
 from util import preprocess
 
-prefix = "/home/parth/sagemaker_deployment/"
-# prefix = "/opt/ml/"
+# prefix = "/home/parth/sagemaker_deployment/"
+prefix = "/opt/ml/"
 
 class SimpleModel():
     predictor = None
@@ -13,14 +13,16 @@ class SimpleModel():
     def get_model(cls):
         "load model for the model files saved during the train process"
         if cls.predictor == None:
-            print("Initialised Model")
+            print("Model initialized")
+            # model path where the model will be located in the sagemaker container
+            # generally it will be located at /opt/ml/model folder of the container
             model_path = os.path.join(prefix, 'model')
             with open("{}/logistic_model.pkl".format(model_path) , "rb") as fl:
                 cls.predictor = pickle.load(fl)
 
             with open("{}/vectorizer.pkl".format(model_path) , "rb") as fl:
                 cls.vectorizer = pickle.load(fl)
-        print("return model")
+            
         return cls.predictor, cls.vectorizer
     
     @classmethod
